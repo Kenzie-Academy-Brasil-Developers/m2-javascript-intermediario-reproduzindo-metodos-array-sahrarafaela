4d88d287ecdb6dbb8cf1e3ae855f4aa693e1e667
sahrarafaela <sararafaela704@gmail.com>
2022-08-02 07:06:30 -0300

    att

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ function callbackReduce(acumulator, valorAtual) {
 }
 
 function reduce(array, callback, initialValue = array[0]) {
-  for (let i = 1; i < array.length; i++) {
+  for (let i = 0; i < array.length; i++) {
     initialValue = callback(initialValue, array[i])
   }
   return initialValue
